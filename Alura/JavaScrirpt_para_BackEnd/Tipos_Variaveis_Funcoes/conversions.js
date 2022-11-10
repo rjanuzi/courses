9e3281c9a5aeaa254d7241541129204ad0cd381d
Rafael B. Januzi <rjanuzi@gmail.com>
2022-11-10 15:21:24 -0300

    Updating JS course

--- Alura/JavaScrirpt_para_BackEnd/Tipos_Variaveis_Funcoes/conversions.js
+++ Alura/JavaScrirpt_para_BackEnd/Tipos_Variaveis_Funcoes/conversions.js
@@ -9,6 +9,9 @@ console.log("123 == '123': " + (num == numStr)); // true - Test the value only
 // Explicit conversions
 console.log("123 + '123': " + (num + numStr)); // 123123
 console.log("123 + Number('123'): " + (num + Number(numStr))); // 246
+console.log("String(123) + '123': " + (String(num) + numStr)); // 123123
+console.log("123.toString() + '123': " + (num.toString() + numStr)); // 123123
+console.log("+ 123 + '123': " + (+ num + numStr)); // 123123
 
 // Invalid number parse generate NaN and not and error
 console.log("Number('a231'): " + Number("a231"))
