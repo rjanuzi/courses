8c301cd919285f0caff663ddaaac1abce5202b6e
Rafael B. Januzi <rjanuzi@gmail.com>
2022-11-27 17:44:13 -0300

    Updating JS Course

--- Alura/JavaScrirpt_para_BackEnd/Algoritmos_Ordenacao/insertion_sort.js
+++ Alura/JavaScrirpt_para_BackEnd/Algoritmos_Ordenacao/insertion_sort.js
@@ -5,6 +5,25 @@ const books = [
   { name: 'Book D', price: 3 }
 ]
 
-function insertionSort (arr) {
-  //TODO - Implement this function
+function swap (arr, idx1, idx2) {
+  let tmp = arr[idx1]
+  arr[idx1] = arr[idx2]
+  arr[idx2] = tmp
 }
+
+function insertionSort (arr) {
+  for (let currentIdx = 0; currentIdx < arr.length; currentIdx++) {
+    let sorterdArrIdx = currentIdx
+    while (
+      sorterdArrIdx > 0 &&
+      arr[sorterdArrIdx].price < arr[sorterdArrIdx - 1].price
+    ) {
+      swap(arr, sorterdArrIdx, sorterdArrIdx - 1)
+      sorterdArrIdx--
+    }
+  }
+}
+
+console.log(books)
+insertionSort(books)
+console.log(books)
