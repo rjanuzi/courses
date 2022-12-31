58c51c3f76d652a50ee74226ab5707e5ff432862
Rafael B. Januzi <rjanuzi@gmail.com>
2022-12-31 19:09:27 -0300

    Alura NodeJS - Adding logic to get links from md file using regex

--- Alura/JavaScrirpt_para_BackEnd/NodeJs/index.js
+++ Alura/JavaScrirpt_para_BackEnd/NodeJs/index.js
@@ -1,8 +1,10 @@
 import fs from "fs"; // Native Node.js module
 import chalk from "chalk"; // Third-party module
 
+const linksRegex = /\[([^[\]]*?)\]\((https?:\/\/[^s?#.].[^\s]*)\)/gm;
+
 function digestReadError(err) {
-  throw new Error(chalk.red(err.code, "File not finded"));
+  throw err;
 }
 
 // Sync version
@@ -27,13 +29,15 @@ function digestReadError(err) {
 // }
 
 // Async/Await - Same result as promisse, but more readable
-async function readFile(path) {
+async function extractLinks(path) {
   try {
     const data = await fs.promises.readFile(path, {
       encoding: "utf-8",
       flag: "r",
     });
-    console.log(chalk.greenBright(data));
+    let links = [...data.matchAll(linksRegex)]; // Spread the result and create the array
+    let result = links.map((l) => ({ name: l[1], url: l[2] })); // We are interested only in the first and second groups
+    console.log(result);
   } catch (err) {
     digestReadError(err);
   } finally {
@@ -41,5 +45,4 @@ async function readFile(path) {
   }
 }
 
-await readFile("files/text.md");
-await readFile("files/texxt.md"); // Error
+await extractLinks("files/text.md");
