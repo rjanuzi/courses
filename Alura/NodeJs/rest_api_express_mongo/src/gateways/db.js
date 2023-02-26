495b15225049883be5470f646f50ec3642fd9931
Rafael B. Januzi <rjanuzi@gmail.com>
2023-02-26 12:22:41 -0300

    Alura Course - Moving config folder

--- Alura/NodeJs/rest_api_express_mongo/src/gateways/db.js
+++ Alura/NodeJs/rest_api_express_mongo/src/gateways/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import fs from "fs";
 
-const DB_CONFIG_PATH = "./config/db-config.json";
+const DB_CONFIG_PATH = "./src/config/db-config.json";
 
 function getMongoDbUri() {
   const dbConfig = JSON.parse(fs.readFileSync(DB_CONFIG_PATH));
