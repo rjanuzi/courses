6080d76aabd35cd2136cb351235f17abf512820a
Rafael B. Januzi <rjanuzi@gmail.com>
2023-03-12 13:59:47 -0300

    ORM_SEQUELIZE - Completing CRUD for Person

--- Alura/ORM_Sequelize/api/controllers/PeopleController.js
+++ Alura/ORM_Sequelize/api/controllers/PeopleController.js
@@ -1,4 +1,5 @@
 const db = require("../models"); /* By default get the index.js */
+const { Op } = require("sequelize");
 
 class PeopleController {
   static async getAllPeople(req, res) {
@@ -8,6 +9,75 @@ class PeopleController {
     } catch (err) {
       res.status(500).send({
         message: `Some error occurred while retrieving persons. - ${err.message}`,
+      });
+    }
+  }
+
+  static async getPersonById(req, res) {
+    try {
+      const person = await db.Person.findByPk(req.params.id);
+      return res.status(200).json(person);
+    } catch (err) {
+      res.status(500).send({
+        message: `Some error occurred while retrieving person. - ${err.message}`,
+      });
+    }
+  }
+
+  static async createPerson(req, res) {
+    try {
+      const personData = req.body;
+      const person = await db.Person.create(personData);
+      return res.status(201).json(person);
+    } catch (err) {
+      res.status(500).send({
+        message: `Some error occurred while creating person. - ${err.message}`,
+      });
+    }
+  }
+
+  static async updatePerson(req, res) {
+    try {
+      const personNewData = req.body;
+      const personId = req.params.id;
+
+      await db.Person.update(personNewData, {
+        where: { id: personId },
+      });
+
+      res.status(201).json(`Person with id ${personId} updated.`);
+    } catch (err) {
+      res.status(500).send({
+        message: `Some error occurred while updating person. - ${err.message}`,
+      });
+    }
+  }
+
+  static async deletePerson(req, res) {
+    try {
+      const personId = req.params.id;
+
+      await db.Person.destroy({
+        where: { id: personId },
+      });
+
+      res.status(201).json(`Person with id ${personId} deleted.`);
+    } catch (err) {
+      res.status(500).send({
+        message: `Some error occurred while deleting person. - ${err.message}`,
+      });
+    }
+  }
+
+  static async getPeopleByName(req, res) {
+    try {
+      const person = await db.Person.findAll({
+        where: { name: { [Op.like]: `%${req.query.name}%` } },
+      });
+      return res.status(200).json(person);
+    } catch (err) {
+      res.status(500).send({
+        message: `Some error occurred while retrieving person. - ${err.message}`,
       });
     }
   }
--- Alura/ORM_Sequelize/api/routes/peopleRouter.js
+++ Alura/ORM_Sequelize/api/routes/peopleRouter.js
@@ -4,5 +4,10 @@ const PeopleController = require("../controllers/PeopleController");
 const router = Router();
 
 router.get("/people", PeopleController.getAllPeople);
+router.get("/people/byName", PeopleController.getPeopleByName);
+router.get("/people/:id", PeopleController.getPersonById);
+router.post("/people", PeopleController.createPerson);
+router.put("/people/:id", PeopleController.updatePerson);
+router.delete("/people/:id", PeopleController.deletePerson);
 
 module.exports = router;
